f0d7df9588533d4715e83a825f48b04a2cf9cf77
danieldawid <daniel.dawid@contractors.roche.com>
2020-05-21 19:37:10 +0200

    Second commit, basics of projects are working

---
+++ src/app/recipe-book/recipe-book.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core"
+
+@Component({
+selector: 'app-recipe-book',
+templateUrl: './recipe-book.component.html',
+styleUrls: ['./recipe-book.component.css']
+})
+
+export class RecipeBookComponent implements OnInit {
+   
+    ngOnInit(): void {
+    }
+    
+}
